25f3843023c2dddeddd0733d1bdcc9340bc4d5f0
Kikepal88 <omarpal88@gmail.com>
2022-08-03 00:11:35 -0500

    Se maqueta el componente del Header, y se agregan algunos estilos básicos del portafolio y del header

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './header.css';
 
 
 function Header(props) {
@@ -6,9 +7,35 @@ function Header(props) {
 
   return(
     <header>
-      <div>
-        <img src={logo} alt="Logo Kike" />
-      </div>
+      <section className="header_section">
+        <div className="header_logo">
+          <img src={logo} alt="Logo Kike" />
+        </div>
+        <div className="header_menu">
+          <i id="header__icon-menu" className="header__icon-menu" onclick="showMobileMenu()"></i>
+          <div id="header_menu_nav" className="header_menu_nav">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#about"><h3>SOBRE MÍ</h3></a>
+                </li>
+                <li>
+                  <a href="#education"><h3>EDUCACIÓN</h3></a>
+                </li>
+                <li>
+                  <a href="#projects"><h3>PROYECTOS</h3></a>
+                </li>
+                <li>
+                  <a href="#technologies"><h3>TECNOLOGÍAS</h3></a>
+                </li>
+                <li>
+                  <a href="#contact"><h3>CONTACTO</h3></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </section>
     </header>
   );
 }

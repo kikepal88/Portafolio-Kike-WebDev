c88ede5495b5970b2d88d0f47508b86b505f9192
Kikepal88 <omarpal88@gmail.com>
2022-08-15 19:11:57 -0500

    Se corrigierón los errores en la lógica del menu, la estructura HTML y algunos estilos del Header

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -3,8 +3,7 @@ import './header.css';
 
 function showMobileMenu () {
   document.getElementById('header_icon-menu').classList.toggle('icon-close');
-  document.getElementById('header_nav').classList.toggle('is-active');
-  document.getElementById('header_section').classList.toggle('menu_is-active');
+  document.getElementById('header_menu-nav').classList.toggle('is-active');
 }
 
 function Header(props) {
@@ -16,33 +15,34 @@ function Header(props) {
         <div className="header_logo">
           <img src={logo} alt="Logo Kike" />
         </div>
-        <div className="header_menu">
-          <span
-            id="header_icon-menu"
-            className="header_icon-menu"
-            onClick={showMobileMenu}>
-          </span>
-          <div id="header_menu-nav" className="header_menu-nav">
-            <nav>
-              <ul>
-                <li>
-                  <a href="#about"><h3>SOBRE MÍ</h3></a>
-                </li>
-                <li>
-                  <a href="#education"><h3>EDUCACIÓN</h3></a>
-                </li>
-                <li>
-                  <a href="#projects"><h3>PROYECTOS</h3></a>
-                </li>
-                <li>
-                  <a href="#technologies"><h3>TECNOLOGÍAS</h3></a>
-                </li>
-                <li>
-                  <a href="#contact"><h3>CONTACTO</h3></a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+        <i
+          id="header_icon-menu"
+          className="header_menu-icon"
+          onClick={showMobileMenu}>
+        </i>
+        <div id="header_menu-nav" className="header_menu-nav">
+          <nav>
+            <ul>
+              <li>
+                <a href="#about"><h3>SOBRE MÍ</h3></a>
+              </li>
+              <li>
+                <a href="#education"><h3>EDUCACIÓN</h3></a>
+              </li>
+              <li>
+                <a href="#projects"><h3>PROYECTOS</h3></a>
+              </li>
+              <li>
+                <a href="#technologies"><h3>TECNOLOGÍAS</h3></a>
+              </li>
+              <li>
+                <a href="#contact"><h3>HABILIDADES</h3></a>
+              </li>
+              <li>
+                <a href="#contact"><h3>CONTACTO</h3></a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </section>
     </header>

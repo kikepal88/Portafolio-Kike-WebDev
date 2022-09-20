6209d7fd4f8e8af9728f0b8e3330953822f3d7bd
Kikepal88 <omarpal88@gmail.com>
2022-09-20 00:20:49 -0500

    Se agregan algunos estilos en la sección de Sobre Mí y de Proyectos, se actualiza el data

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -18,6 +18,8 @@ function Projects (props) {
                 description,
                 img_mobile,
                 img_desktop,
+                url_repo,
+                url_demo,
                 technologies,
               } = project;
               return(
@@ -30,14 +32,27 @@ function Projects (props) {
                       <img src={img_desktop} alt={`Desktop_${name}`}/>
                     </div>
                   </div>
+                  <div className="img_iterator">
+                    <article className="iterator_img">
+                      <img src="https://firebasestorage.googleapis.com/v0/b/base-de-imagenes-proyectos.appspot.com/o/mobile2.svg?alt=media&token=4b7196a5-f4e6-41f9-a0d7-288b11bd3a1f" alt="icono phone"/>
+                    </article>
+                    <article className="iterator_img">
+                      <img src="https://firebasestorage.googleapis.com/v0/b/base-de-imagenes-proyectos.appspot.com/o/display.svg?alt=media&token=38ed380f-bc20-4032-95de-859117916d26" alt="icono display"/>
+                    </article>
+                    <span></span>
+                  </div>
                   <div className="container_description">
                     <p>{description}</p>
                     <div className="description_buttons">
                       <div className="buttons">
-                        <button>REPO</button>
+                        <a href={url_repo} target="_blank" rel="noreferrer">
+                          <button>REPO</button>
+                        </a>
                       </div>
                       <div className="buttons">
-                        <button>DEMO</button>
+                        <a href={url_demo} target="_blank" rel="noreferrer">
+                          <button>DEMO</button>
+                        </a>
                       </div>
                     </div>
                     <div className="description_technologies">
